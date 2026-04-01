b4e8cd1e01fd15d0b9585756df9a3ca581d85652
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-01 15:41:25 -0400

    fix(prompt): replace loom profile on/off with profile::mod using p6_return_words and add README (#1)
    * fix(prompt): replace loom profile on/off with profile::mod using p6_return_words and add README
    
    * fix(prompt): use double quotes for variable expansion in p6_return_words (SC2016)
    
    * chore(ci): add standard github workflows
    
    * fix(loom): restore single-quoted variable in profile::mod

--- init.zsh
+++ init.zsh
@@ -7,74 +7,21 @@
 #>
 ######################################################################
 p6df::modules::loom::deps() {
-  ModuleDeps=()
+  ModuleDeps=(p6m7g8-dotfiles/p6common)
 }
 
 ######################################################################
 #<
 #
-# Function: str str = p6df::modules::loom::prompt::mod()
+# Function: words loom $LOOM_API_KEY = p6df::modules::loom::profile::mod()
 #
 #  Returns:
-#	str - str
+#	words - loom $LOOM_API_KEY
 #
-#  Environment:	 LOOM_API_KEY P6_DFZ_PROFILE_LOOM
+#  Environment:	 LOOM_API_KEY
 #>
 ######################################################################
-p6df::modules::loom::prompt::mod() {
-  local str=""
-  local profile="$P6_DFZ_PROFILE_LOOM"
-  local api_key="$LOOM_API_KEY"
+p6df::modules::loom::profile::mod() {
 
-  if p6_string_blank_NOT "$profile"; then
-    str="loom:\t\t  ${profile}:"
-    if p6_string_blank_NOT "$api_key"; then
-      str=$(p6_string_append "$str" "api" " ")
-    fi
-  fi
-
-  p6_return_str "$str"
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::loom::profile::on(profile, code)
-#
-#  Args:
-#	profile -
-#	code - shell code block (export LOOM_API_KEY=...)
-#
-#  Environment:	 LOOM_API_KEY P6_DFZ_PROFILE_LOOM
-#>
-######################################################################
-p6df::modules::loom::profile::on() {
-  local profile="$1"
-  local code="$2"
-
-  p6_run_code "$code"
-
-  p6_env_export "P6_DFZ_PROFILE_LOOM" "$profile"
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::loom::profile::off(code)
-#
-#  Args:
-#	code - shell code block previously passed to profile::on
-#
-#  Environment:	 LOOM_API_KEY P6_DFZ_PROFILE_LOOM
-#>
-######################################################################
-p6df::modules::loom::profile::off() {
-  local code="$1"
-
-  p6_env_unset_from_code "$code"
-  p6_env_export_un P6_DFZ_PROFILE_LOOM
-
-  p6_return_void
+  p6_return_words 'loom' '$LOOM_API_KEY'
 }
